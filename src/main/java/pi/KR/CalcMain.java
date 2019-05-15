a3704a2fd86309b6180e25abdb28ea3ac1cc7f56
Author <Author@Author>
2019-05-16 00:12:22 +0300

    0:12 16.05.19

--- src/main/java/pi/KR/CalcMain.java
+++ src/main/java/pi/KR/CalcMain.java
@@ -161,10 +161,10 @@ class CalcMain extends JFrame {
      * imageStringWfRectangle - строка со значением площади оконной рамы, полученным после проверки и вычисления (Прямоугольная ОК);
      * imageStringWfRound - строка со значением площади оконной рамы, полученным после проверки и вычисления (Круглая ОК);
      * imageStringWfOval - строка со значением площади оконной рамы, полученным после проверки и вычисления (Овальная ОК). */
-    JLabel labelType = new JLabel("Выберите оконную конструкцию");
-    JLabel labelHeight = new JLabel("Высота (см)");
-    JLabel labelWeight = new JLabel("Ширина (см)");
-    JLabel labelRadius = new JLabel("Радиус (см)");
+    JLabel labelType = new JLabel("Choose window");
+    JLabel labelHeight = new JLabel("Height");
+    JLabel labelWeight = new JLabel("Weight");
+    JLabel labelRadius = new JLabel("Radius");
     JLabel labelMaterial = new JLabel("Материал");
     JLabel labelSealing = new JLabel("Уплотнение");
     JLabel labelAnswer1 = new JLabel("");
