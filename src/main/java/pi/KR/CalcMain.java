91175a2df5f90a582af51831d6502e36cedc67e7
Maxim <kitakk17@gmail.com>
2019-05-21 09:14:56 +0300

    Update CalcMain.java

--- src/main/java/pi/KR/CalcMain.java
+++ src/main/java/pi/KR/CalcMain.java
@@ -162,3 +162,4 @@ class CalcMain extends JFrame {
      * imageStringWfRound - строка со значением площади оконной рамы, полученным после проверки и вычисления (Круглая ОК);
      * imageStringWfOval - строка со значением площади оконной рамы, полученным после проверки и вычисления (Овальная ОК). */
     JLabel labelType = new JLabel("Выберите оконную конструкцию");
+    JLabel labelHeight = new JLabel("Высота (см)");
