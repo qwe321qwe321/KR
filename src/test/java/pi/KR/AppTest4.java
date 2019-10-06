7f56042fd5f2ec8ab42c110ecdd7002fe45e84a3
qwe321qwe321 <50456533+qwe321qwe321@users.noreply.github.com>
2019-10-06 23:22:57 +0300

    Update AppTest4.java

--- src/test/java/pi/KR/AppTest4.java
+++ src/test/java/pi/KR/AppTest4.java
@@ -4,12 +4,13 @@ import org.junit.jupiter.api.Test;
 
 /* Метод, тестирующий вычисления ОК прямоугольного типа, используя метод assertEquals(),
  * принимающий в качестве параметров ответ (число) и число, возвращенное методом calculation(). */
-public class AppTest4 {
+public class AppTest1 {
 	
 	CalcMain api = new CalcMain();
-    @Test
-    public void testApp() {
-        assertEquals(441, api.calculation(20, 20));
-    }
-    
+	@Test
+   	
+	public void testApp() {
+		assertEquals(441, api.calculation(20, 20));
+	}
+	
 }
